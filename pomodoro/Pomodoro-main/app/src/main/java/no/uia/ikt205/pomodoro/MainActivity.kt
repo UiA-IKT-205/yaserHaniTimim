cf9ed5bfb2ba39acad4ac928435ce1c63c9aa521
yaser tamim <yassertamem84@gmail.com>
2021-01-25 13:25:42 +0100

    new attempt

--- pomodoro/Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ pomodoro/Pomodoro-main/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -15,14 +15,42 @@ class MainActivity : AppCompatActivity() {
     lateinit var startButton:Button
     lateinit var coutdownDisplay:TextView
 
-    val timeToCountDownInMs = 5000L
+    lateinit var btn30:Button
+    lateinit var btn60:Button
+    lateinit var btn90:Button
+    lateinit var btn120:Button
+
+    var timeToCountDownInMs =0L
     val timeTicks = 1000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
        startButton = findViewById<Button>(R.id.startCountdownButton)
+
+        btn30 = findViewById<Button>(R.id.btn30)
+        btn60 = findViewById<Button>(R.id.btn60)
+        btn90 = findViewById<Button>(R.id.btn90)
+        btn120 = findViewById<Button>(R.id.btn120)
+
+        btn30.setOnClickListener(){
+            timeToCountDownInMs=1800000
+        }
+
+        btn60.setOnClickListener(){
+            timeToCountDownInMs=3600000
+        }
+
+        btn90.setOnClickListener(){
+            timeToCountDownInMs=5400000
+        }
+
+        btn120.setOnClickListener(){
+            timeToCountDownInMs=720000
+        }
+
        startButton.setOnClickListener(){
            startCountDown(it)
        }
